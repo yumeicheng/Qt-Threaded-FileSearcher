60d5fcd16f507b08076d35f1772300af0c2b8b5c
qlcymc <qlcymc@126.com>
2026-01-15 18:16:23 +0800

    Feat: Add Dark Theme UI and fix CMake resource config

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QDir>
+#include <QDebug>
+
 
 int main(int argc, char *argv[])
 {
+    // 打印根目录下的所有资源
+    QDir dir(":/");
+    qDebug() << "资源根目录下的东西: " << dir.entryList();
+
+    // 打印 new 下面的东西
+    QDir dir2(":/new");
+    qDebug() << "new 下面的东西: " << dir2.entryList();
     QApplication a(argc, argv);
+
+    QFile file(":/style.qss");
+    if(file.open(QFile::ReadOnly))
+    {
+        QString styleSheet = QLatin1String(file.readAll());
+        a.setStyleSheet(styleSheet);
+        file.close();
+    }
+    else{
+        qDebug() << "样表加载失败！" ;
+    }
     MainWindow w;
     w.show();
     return a.exec();
